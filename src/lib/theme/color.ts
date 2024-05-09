12cc5cbf2f309b6d49c1c467203b80d1b82f3d87
Shuta Kumano <shoota.dev@gmail.com>
2024-05-10 02:25:43 +0900

    feat: add colors as RGB

--- src/lib/theme/color.ts
+++ src/lib/theme/color.ts
@@ -6,3 +6,12 @@ export const colors = {
   dark: "#04252B",
   tone: "#CFD8DE", // light of default
 }
+
+export const colorsRGB = {
+  base: "51, 51, 51",
+  main: "53, 67, 76",
+  default: "153, 153, 153",
+  light: "214, 214, 214",
+  dark: "4, 37, 43",
+  tone: "207, 216, 222",
+}
