ab9ab27deea6abbc5c55a16f21fb58e1c8c4832e
Shuta Kumano <shoota.dev@gmail.com>
2024-05-11 00:34:35 +0900

    feat: fix Hero

--- src/lib/components/Hero/Hero.stories.tsx
+++ src/lib/components/Hero/Hero.stories.tsx
@@ -1,3 +1,4 @@
+import { action } from "@storybook/addon-actions"
 import type { Meta, StoryObj } from "@storybook/react"
 import { Hero } from "./Hero"
 
@@ -8,6 +9,12 @@ const meta = {
     title: "Hello Everyone, I'm a cat.",
     description: "I'm a cat, and I'm here to say hello to you.",
     image: { src: "/image/example.jpg" },
+    imageCaption: (
+      <>
+        Photo by <a>@shoota</a> in 2024
+      </>
+    ),
+    onClick: action("onClick"),
   },
 } satisfies Meta<typeof Hero>
 
