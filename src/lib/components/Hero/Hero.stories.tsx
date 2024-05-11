05ee19072985e1c49300704a472748685e781bef
Shuta Kumano <shoota.dev@gmail.com>
2024-05-12 00:42:14 +0900

    feat: strict props to use card as Hero component

--- src/lib/components/Hero/Hero.stories.tsx
+++ src/lib/components/Hero/Hero.stories.tsx
@@ -1,4 +1,3 @@
-import { action } from "@storybook/addon-actions"
 import type { Meta, StoryObj } from "@storybook/react"
 import { Hero } from "./Hero"
 
@@ -7,14 +6,12 @@ const meta = {
   component: Hero,
   args: {
     title: "Hello Everyone, I'm a cat.",
-    description: "I'm a cat, and I'm here to say hello to you.",
     image: { src: "/image/example.jpg" },
     imageCaption: (
       <>
         Photo by <a>@shoota</a> in 2024
       </>
     ),
-    onClick: action("onClick"),
   },
 } satisfies Meta<typeof Hero>
 
