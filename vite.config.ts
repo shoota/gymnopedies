982add2b9b6a9431144508c5c55452c971e95829
Shuta Kumano <shoota.dev@gmail.com>
2024-05-16 01:29:47 +0900

    chore: use swc plugin and emotion plugin to build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { resolve } from "path"
 import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      plugins: [["@swc/plugin-emotion", {}]],
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/lib/index.ts"),
