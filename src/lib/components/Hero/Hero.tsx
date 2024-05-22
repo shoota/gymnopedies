9809140e5056d091a4c88850eebbd49f9f715292
Shuta Kumano <shoota.dev@gmail.com>
2024-05-23 00:37:29 +0900

    feat: implement Hero

--- src/lib/components/Hero/Hero.tsx
+++ src/lib/components/Hero/Hero.tsx
@@ -1,20 +1,47 @@
-import { ComponentProps, PropsWithChildren } from "react"
-import { Card } from "../../core"
+import { ImgHTMLAttributes, PropsWithChildren } from "react"
+import styled from "@emotion/styled"
+import { colorsRGB } from "../../theme/color"
 
-type CardProps = ComponentProps<typeof Card>
-
-type Props = Pick<CardProps, "image" | "imageCaption" | "title">
-export const Hero = ({
-  title,
-  image,
-  imageCaption,
-}: PropsWithChildren<Props>) => {
+type Props = { cover: string } & ImgHTMLAttributes<HTMLImageElement>
+export const Hero = ({ cover, src }: PropsWithChildren<Props>) => {
   return (
-    <Card
-      title={title}
-      image={image}
-      imageCaption={imageCaption}
-      transition={false}
-    />
+    <FlexContainer>
+      <Container>
+        <Image src={src} />
+      </Container>
+      <Title>{cover}</Title>
+    </FlexContainer>
   )
 }
+
+const FlexContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const Container = styled.div`
+  width: 100%;
+`
+const Image = styled.img`
+  display: block;
+  vertical-align: top;
+  object-fit: cover;
+  object-position: center;
+  opacity: 0.6;
+  filter: grayscale(1);
+`
+
+const Title = styled.h1`
+  display: inline-block;
+  width: 100%;
+  position: absolute;
+  top: 66%;
+  left: 50%;
+  text-align: center;
+  text-wrap: nowrap;
+  background-color: rgba(${colorsRGB.dark}, 0.45);
+  overflow: hidden;
+  font-size: clamp(4px, 6vw, 4.5rem);
+  transform: translate(-50%, -50%);
+`
