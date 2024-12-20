4e45b46e71dd1bc3ba1c5620e24801b6e1e991e6
Shuta Kumano <shoota.dev@gmail.com>
2024-12-20 20:47:12 +0900

    feat: a Picture component can set specific height and object-position

--- src/lib/core/Picture/Picture.stories.tsx
+++ src/lib/core/Picture/Picture.stories.tsx
@@ -5,13 +5,12 @@ const meta = {
   title: "core/Picture",
   component: Picture,
   args: {
-    image: { src: "/image/example.jpg" },
+    image: { src: "/image/example.jpg", height: undefined, transition: false },
     imageCaption: (
       <>
         Photo by <a>@shoota</a> in 2024
       </>
     ),
-    transition: false,
   },
 } satisfies Meta<typeof Picture>
 
@@ -19,3 +18,13 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
+
+export const SpecificHeight: Story = {
+  args: {
+    image: {
+      src: "/image/example.jpg",
+      height: "200px",
+      objectPosition: "0 20%",
+    },
+  },
+}
